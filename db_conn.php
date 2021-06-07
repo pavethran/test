10f699a2616fa713ade802760d3bae7137ccbc3b
pavethran <pavethrancool99@gmail.com>
2021-06-07 11:03:14 +0530

    db updated

---
+++ db_conn.php
@@ -0,0 +1,11 @@
+<?php
+$name = "localhost";
+$uname = "root";
+$password = "";
+$db_name = "test_db";
+
+$conn = mysqli_connect($name,$uname,$password,$db_name);
+
+if(!$conn){
+    echo "connection Failed";
+}
