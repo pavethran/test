149ecee624f53f836480c7f3e244e8a223d086e5
pavethran <pavethrancool99@gmail.com>
2021-06-07 09:48:08 +0530

    CHANGES home page

---
+++ home.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+if(isset($_SESSION['id'])&& isset($_SESSION['email'])){
+?>
+<!DOCTYPE html
+<html>
+<head>
+<title> Home</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<h1> Hello <?php echo $_SESSION['email']; ?> </h1>
+<a href="logout.php">Logout</a>
+</body>
+</html>
+<?php
+}
+else{
+    header("Location: index.php");
+    exit();
+}
+?>
