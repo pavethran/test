4df63ac19cdf851eae8927cf0d27954a09bf70d4
pavethran <pavethrancool99@gmail.com>
2021-06-07 11:08:51 +0530

    changed index.php

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-< action="login.php"method = "post">
+
+<form action="login.php"method = "post">
 <h2>LOGIN</h2>
 <?php if(isset($_GET['error'])){ ?>
 <p class="error"> <?php echo $_GET['error']; ?></p>
